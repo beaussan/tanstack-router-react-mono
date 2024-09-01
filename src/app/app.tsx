5efe24b41727389a41aa65e34153599378e0b0c1
Nicolas Beaussart <nic.beaussart@gmail.com>
2024-09-01 15:27:03 +0200

    Initial commit

---
+++ src/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="tanstack-router-react-mono" />
+    </div>
+  );
+}
+
+export default App;
